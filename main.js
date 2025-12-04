fb367b1b328f71303faffa544b7d35f59b49c4d8
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-04 12:48:46 +0200

    Add 5 small improvements: viewport meta tag, basic styling, enhanced comments, improved .gitignore, and window title

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: 'Minimal Repro',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
--- renderer.js
+++ renderer.js
@@ -4,4 +4,6 @@
  * available in this process because `nodeIntegration` is turned off and
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
+ * 
+ * Add your renderer process code here.
  */
